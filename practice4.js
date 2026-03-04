e67a765f8a7158c9ab8c256b3d3362996acac3cd
unknown <st4924@utr.edu.mx>
2026-03-03 18:09:25 -0600

    Using functions without the arrow

---
+++ practice4.js
@@ -0,0 +1,28 @@
+let a = 10;
+let b = 5;
+
+substraction()
+multiplication()
+division()
+addition()
+
+
+function substraction(){
+    c = a - b
+    console.log(`Substraction: ${a} - ${b} = ${c}`)
+}
+
+function multiplication(){
+    c = a * b
+    console.log(`Multiplication: ${a} * ${b} = ${c}`)
+}
+
+function division(){
+    c = a / b
+    console.log(`Division: ${a} / ${b} = ${c}`)
+}
+
+function addition(){
+    c = a + b
+    console.log(`Addition: ${a} + ${b} = ${c}`)
+}
