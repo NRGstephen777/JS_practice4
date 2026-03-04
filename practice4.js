d282a1cdf398d2ef030aaf4e532b6fa56f6e7f4a
unknown <st4924@utr.edu.mx>
2026-03-03 18:30:10 -0600

    Now, using arrow functions

--- practice4.js
+++ practice4.js
@@ -1,29 +1,16 @@
 let a = 10;
 let b = 5;
+let c = 0;
+
+const substraction = () => console.log(`Substraction: ${a} - ${b} = ${c = a - b}`)
+
+const multiplication = () => console.log(`Multiplication: ${a} * ${b} = ${c = a * b}`)
+
+const division = () => console.log(`Division: ${a} / ${b} = ${c = a / b}`);
+
+const addition = () => console.log(`Addition: ${a} + ${b} = ${c = a + b}`);
 
 substraction()
 multiplication()
 division()
 addition()
-
-
-const substraction = () => {
-    c = a - b
-    console.log(`Substraction: ${a} - ${b} = ${c}`)
-}
-
-const multiplication=()=>{
-    c = a * b
-    console.log(`Multiplication: ${a} * ${b} = ${c}`)
-}
-
-const division=()=>{
-    c = a / b
-    console.log(`Division: ${a} / ${b} = ${c}`);
-}
-
-const addition =() =>{
-    c = a + b;
-    console.log(`Addition: ${a} + ${b} = ${c}`);
-}
-
